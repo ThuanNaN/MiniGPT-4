73d92055e1ee1dd60c20e4ba280ffdb35e1a191e
xxnithicxx <nguyenphucthinh106@gmail.com>
2025-01-14 10:00:12 +0700

    feat: :bug: Change the metrics to mAP, and handle new JSON file format.

--- eval.py
+++ eval.py
@@ -12,22 +12,38 @@ import torch
 from torch.utils.data import DataLoader
 from datasets import load_dataset
 
+from torchmetrics.detection.mean_ap import MeanAveragePrecision
+
 from minigpt4.datasets.datasets.vqa_datasets import RefADEvalData
 from minigpt4.common.vqa_tools.VQA.PythonHelperTools.vqaTools.vqa import VQA
-from minigpt4.common.vqa_tools.VQA.PythonEvaluationTools.vqaEvaluation.vqaEval import VQAEval
+from minigpt4.common.vqa_tools.VQA.PythonEvaluationTools.vqaEvaluation.vqaEval import (
+    VQAEval,
+)
 
-from minigpt4.common.eval_utils import prepare_texts, init_model, eval_parser, computeIoU
+from minigpt4.common.eval_utils import (
+    prepare_texts,
+    init_model,
+    eval_parser,
+    computeIoU,
+)
 from minigpt4.conversation.conversation import CONV_VISION_minigptv2
 from minigpt4.common.config import Config
 
 
 def list_of_str(arg):
-    return list(map(str, arg.split(',')))
+    return list(map(str, arg.split(",")))
+
 
 parser = eval_parser()
-parser.add_argument("--dataset", type=list_of_str, default='refcoco', help="dataset to evaluate")
-parser.add_argument("--resample", action="store_true", default=False, help="resample failed samples")
-parser.add_argument("--res", type=float, default=100.0, help="resolution used in regresion")
+parser.add_argument(
+    "--dataset", type=list_of_str, default="refcoco", help="dataset to evaluate"
+)
+parser.add_argument(
+    "--resample", action="store_true", default=False, help="resample failed samples"
+)
+parser.add_argument(
+    "--res", type=float, default=100.0, help="resolution used in regresion"
+)
 args = parser.parse_args()
 cfg = Config(args)
 
@@ -37,65 +53,40 @@ conv_temp.system = ""
 model.eval()
 save_path = cfg.run_cfg.save_path
 
-if 'mvtech' in args.dataset:
+if "mvtech" in args.dataset:
     eval_file_path = cfg.evaluation_datasets_cfg["mvtech_ad"]["eval_file_path"]
     batch_size = cfg.evaluation_datasets_cfg["mvtech_ad"]["batch_size"]
     max_new_tokens = cfg.evaluation_datasets_cfg["mvtech_ad"]["max_new_tokens"]
-    
-    mvtech_ad = []
-    
+
     # Adapt the data loading to the RefCOCO format
-    mvtech_ad_data_for_regression = []
-    for category in os.listdir(eval_file_path):
-        category_path = os.path.join(eval_file_path, category)
-        if os.path.isdir(category_path):
-            for split in ["test"]: 
-                split_path = os.path.join(category_path, split)
-                if os.path.isdir(split_path):
-                    for defect in os.listdir(split_path):
-                        defect_path = os.path.join(split_path, defect)
-                        for img_file in os.listdir(defect_path):
-                            img_path = os.path.join(defect_path, img_file)
-                            mask_path = os.path.join(category_path, "ground_truth", defect, img_file.replace(".png", "_mask.png"))
+    with open(eval_file_path, "r") as f:
+        mvtech_ad_data_for_regression = json.load(f)
 
-                            if os.path.exists(mask_path):
-                                # Get bounding box from mask
-                                mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-                                contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                                if contours:
-                                    x, y, w, h = cv2.boundingRect(contours[0])
-                                    bbox = [x, y, x + w, y + h]
-
-                                    img_id = f"{category}_{split}_{defect}_{img_file}" 
-                                    
-                                    mvtech_ad_data_for_regression.append({
-                                        "img_id": img_id,
-                                        "img_path": img_path,
-                                        "category": category,
-                                        "defect": defect,
-                                        "bbox": bbox,
-                                        "height": mask.shape[0],  # Assuming mask dimensions match image
-                                        "width": mask.shape[1],
-                                        "sents": '[refer] give me the location of ' + defect.replace('_', ' ') + ' defect'
-                                    })
-                            
+    # Limit the data to 10% of the original data
+    mvtech_ad_data_for_regression = mvtech_ad_data_for_regression[:len(mvtech_ad_data_for_regression)//10]
+    # mvtech_ad_data_for_regression = mvtech_ad_data_for_regression[:10]
     data = RefADEvalData(mvtech_ad_data_for_regression, vis_processor)
-    data = list(data)[:len(data)//10]  # Limit to 10% of the data
     eval_dataloader = DataLoader(data, batch_size=batch_size, shuffle=False)
 
     minigpt4_predict = defaultdict(list)
     resamples = []
 
-    for images, questions, img_ids in tqdm(eval_dataloader):
+    for images, questions, img_ids, labels, image_paths in tqdm(eval_dataloader):
         texts = prepare_texts(questions, conv_temp)
-        answers = model.generate(images, texts, max_new_tokens=max_new_tokens, do_sample=False)
-        for answer, img_id, question in zip(answers, img_ids, questions):
-            answer = answer.replace("<unk>","").replace(" ","").strip()
-            pattern = r'\{<\d{1,3}><\d{1,3}><\d{1,3}><\d{1,3}>\}'
+        answers = model.generate(
+            images, texts, max_new_tokens=max_new_tokens, do_sample=False
+        )
+        for answer, img_id, question, labels, image_paths in zip(
+            answers, img_ids, questions, labels, image_paths
+        ):
+            answer = answer.replace("<unk>", "").replace(" ", "").strip()
+            pattern = r"<p>(.*?)<\/p>\{<\d{1,3}><\d{1,3}><\d{1,3}><\d{1,3}>\}"
             if re.match(pattern, answer):
                 minigpt4_predict[img_id].append(answer)
             else:
-                resamples.append({'img_id': img_id, 'sents': question.replace('[refer] give me the location of','').strip()})
+                resamples.append(
+                    {"img_id": img_id, "class": labels, "img_path": image_paths}
+                )
 
     if args.resample:
         for i in range(20):
@@ -103,51 +94,134 @@ if 'mvtech' in args.dataset:
             resamples = []
             eval_dataloader = DataLoader(data, batch_size=batch_size, shuffle=False)
 
-            for images, questions, img_ids in tqdm(eval_dataloader):
+            for images, questions, img_ids, labels, image_paths in tqdm(
+                eval_dataloader
+            ):
                 texts = prepare_texts(questions, conv_temp)
-                answers = model.generate(images, texts, max_new_tokens=max_new_tokens, do_sample=False)
-                for answer, img_id, question in zip(answers, img_ids, questions):
-                    answer = answer.replace("<unk>","").replace(" ","").strip()
-                    pattern = r'\{<\d{1,3}><\d{1,3}><\d{1,3}><\d{1,3}>\}'
+                answers = model.generate(
+                    images, texts, max_new_tokens=max_new_tokens, do_sample=False
+                )
+                for answer, img_id, question, labels, image_paths in zip(
+                    answers, img_ids, questions, labels, image_paths
+                ):
+                    answer = answer.replace("<unk>", "").replace(" ", "").strip()
+                    pattern = r"<p>(.*?)<\/p>\{<\d{1,3}><\d{1,3}><\d{1,3}><\d{1,3}>\}"
                     if re.match(pattern, answer) or i == 4:
                         minigpt4_predict[img_id].append(answer)
                     else:
-                        resamples.append({'img_id': img_id, 'sents': question.replace('[refer] give me the location of','').strip()})
+                        resamples.append(
+                            {"img_id": img_id, "class": labels, "img_path": image_paths}
+                        )
 
             if len(resamples) == 0:
                 break
 
     # Save predictions
     file_save_path = os.path.join(save_path, "mvtech_ad_regression.json")
-    with open(file_save_path, 'w') as f:
+    with open(file_save_path, "w") as f:
         json.dump(minigpt4_predict, f)
+
+    metric = MeanAveragePrecision(iou_type="bbox", class_metrics=True)
 
     # Calculate metrics
     count = 0
-    total = len(mvtech_ad_data_for_regression)
+    total = 0
     res = args.res
     for item in mvtech_ad_data_for_regression:
-        img_id = item['img_id']
-        bbox = item['bbox']
+        img_id = (
+            item["class"] + "." + os.path.basename(item["image_path"]).split(".")[0]
+        )
+        label = item["class"]
+        is_broken = item["is_broken"]
         outputs = minigpt4_predict[img_id]
 
+        # Determine ground truth bounding box and class
+        if not is_broken:
+            # If not broken, the bounding box is the whole image
+            gt_bbox = [0, 0, item["width"], item["height"]]
+            gt_class = 0  # Class 0 for "not-defect"
+        else:
+            gt_bbox = [
+                item["bbox"][0],
+                item["bbox"][1],
+                item["bbox"][2],
+                item["bbox"][3],
+            ]
+            gt_class = 1  # Class 1 for "defect"
+
+        # Ground truth data for torchmetrics
+        gt_boxes = torch.tensor([gt_bbox], dtype=torch.float)
+        gt_labels = torch.tensor([gt_class], dtype=torch.int)
+
+        outputs = minigpt4_predict[img_id]
+
+        pred_boxes = []
+        pred_scores = []
+        pred_labels = []
+
         for output in outputs:
-            try:
-                integers = re.findall(r'\d+', output)
-                pred_bbox = [int(num) for num in integers]
-                height = item['height']
-                width = item['width']
-                pred_bbox[0] = pred_bbox[0] / res * width
-                pred_bbox[1] = pred_bbox[1] / res * height
-                pred_bbox[2] = pred_bbox[2] / res * width
-                pred_bbox[3] = pred_bbox[3] / res * height
+            match = re.search(
+                r"<p>(.*?)<\/p>\{<(\d{1,3})><(\d{1,3})><(\d{1,3})><(\d{1,3})>\}",
+                output,
+            )
+            if match:
+                try:
+                    pred_class_str = match.group(1).strip()
 
-                gt_bbox = [bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]]
+                    # Determine predicted class based on the string
+                    if "not-defect" in pred_class_str:
+                        pred_class = 0
+                    elif "defect" in pred_class_str:
+                        pred_class = 1
+                    else:
+                        pred_class = -1  # Or some other value to indicate uncertain
 
-                iou_score = computeIoU(pred_bbox, gt_bbox)
-                if iou_score > 0.5:
-                    count += 1
-            except:
-                continue
+                    pred_bbox = [
+                        int(match.group(2)),
+                        int(match.group(3)),
+                        int(match.group(4)),
+                        int(match.group(5)),
+                    ]
+                    height = item["height"]
+                    width = item["width"]
+                    pred_bbox[0] = pred_bbox[0] / res * width
+                    pred_bbox[1] = pred_bbox[1] / res * height
+                    pred_bbox[2] = pred_bbox[2] / res * width
+                    pred_bbox[3] = pred_bbox[3] / res * height
 
-    print(f'MVTech AD (Regression):', count / total * 100, flush=True)
+                    pred_boxes.append(pred_bbox)
+                    pred_scores.append(1.0)  # Assuming confidence score of 1
+                    pred_labels.append(pred_class)
+                except Exception as e:
+                    print(f"Error processing output: {output}, Error: {e}")
+                    continue
+
+        # Convert lists to tensors
+        if pred_boxes:
+            pred_boxes = torch.tensor(pred_boxes, dtype=torch.float)
+            pred_scores = torch.tensor(pred_scores, dtype=torch.float)
+            pred_labels = torch.tensor(pred_labels, dtype=torch.int)
+        else:
+            # Create empty tensors if no predictions were made
+            pred_boxes = torch.empty((0, 4), dtype=torch.float)
+            pred_scores = torch.empty(0, dtype=torch.float)
+            pred_labels = torch.empty(0, dtype=torch.int)
+
+        # Update metric
+        metric.update(
+            [dict(boxes=pred_boxes, scores=pred_scores, labels=pred_labels)],
+            [dict(boxes=gt_boxes, labels=gt_labels)],
+        )
+
+    # Compute metric
+    result = metric.compute()
+    map_value = result["map"].item()
+
+    # Print class-wise metrics
+    for i, class_map in enumerate(result["map_per_class"]):
+        class_name = "defect" if i == 1 else "not-defect"
+        print(
+            f"mAP for {class_name}: {class_map.item() * 100 if not torch.isnan(class_map) else 0:.4f}",
+            flush=True,
+        )
+    print(f"mAP: {map_value * 100}", flush=True)
--- minigpt4/datasets/datasets/vqa_datasets.py
+++ minigpt4/datasets/datasets/vqa_datasets.py
@@ -5,11 +5,12 @@
  For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 """
 
+import os
 import torch
 from PIL import Image
-import os
 
 from minigpt4.datasets.datasets.base_dataset import BaseDataset
+
 
 class RefADEvalData(torch.utils.data.Dataset):
     def __init__(self, loaded_data, vis_processor):
@@ -21,12 +22,18 @@ class RefADEvalData(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         data = self.loaded_data[idx]
-        sent = data["sents"]
-        img_id = data["img_id"]
-        image_path = data["img_path"]
-        image = Image.open(image_path).convert("RGB")
+        sent = "[detection] a defect or not-defect object and return the bounding boxes and its label. If not, bound around the object."
+        img_id = (
+            data["class"] + "." + os.path.basename(data["image_path"]).split(".")[0]
+        )
+        # image_path = data["image_path"]
+        fix_path = os.path.join(
+            "./MVTEC_det/images", "/".join(data["image_path"].split("/")[1:4])
+        )
+        image = Image.open(fix_path).convert("RGB")
         image = self.vis_processor(image)
-        return image, sent, img_id
+        return image, sent, img_id, data["class"], data["image_path"]
+
 
 class VQADataset(BaseDataset):
     def __init__(self, vis_processor, text_processor, vis_root, ann_paths):
@@ -46,18 +53,19 @@ class OKVQAEvalData(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.loaded_data)
-    
+
     def __getitem__(self, idx):
         data = self.loaded_data[idx]
-        img_id = data['image_id']
-        question = data['question']
-        question_id = data['question_id']
-        img_file = '{:0>12}.jpg'.format(img_id)
+        img_id = data["image_id"]
+        question = data["question"]
+        question_id = data["question_id"]
+        img_file = "{:0>12}.jpg".format(img_id)
         image_path = os.path.join(self.root_path, img_file)
-        image = Image.open(image_path).convert('RGB')
+        image = Image.open(image_path).convert("RGB")
         image = self.vis_processor(image)
         question = f"[vqa] Based on the image, respond to this question with a short answer: {question}"
         return image, question, question_id, img_id
+
 
 class VizWizEvalData(torch.utils.data.Dataset):
     def __init__(self, loaded_data, vis_processor, root_path):
@@ -67,18 +75,19 @@ class VizWizEvalData(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.loaded_data)
-    
+
     def __getitem__(self, idx):
         data = self.loaded_data[idx]
-        img_id = data['image']
-        question = data['question']
-        answers = data['answers']
-        answers = '_'.join([answer['answer'] for answer in answers])
+        img_id = data["image"]
+        question = data["question"]
+        answers = data["answers"]
+        answers = "_".join([answer["answer"] for answer in answers])
         image_path = os.path.join(self.root_path, img_id)
-        image = Image.open(image_path).convert('RGB')
+        image = Image.open(image_path).convert("RGB")
         image = self.vis_processor(image)
         question = f"[vqa] The question is '{question}' Based on the image, answer the question with a single word or phrase. and reply 'unanswerable' when the provided information is insufficient"
         return image, question, answers
+
 
 class IconQAEvalData(torch.utils.data.Dataset):
     def __init__(self, loaded_data, vis_processor, root_path):
@@ -88,18 +97,19 @@ class IconQAEvalData(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.loaded_data)
-    
+
     def __getitem__(self, idx):
         data = self.loaded_data[idx]
-        image_id = data['image_id']
-        question = data['question']
-        image_path = os.path.join(self.root_path, image_id, 'image.png')
-        image = Image.open(image_path).convert('RGB')
+        image_id = data["image_id"]
+        question = data["question"]
+        image_path = os.path.join(self.root_path, image_id, "image.png")
+        image = Image.open(image_path).convert("RGB")
         image = self.vis_processor(image).half().cuda()
-        candidates = '_'.join(data['choices'])
-        answer = data['answer']
+        candidates = "_".join(data["choices"])
+        answer = data["answer"]
         question = f"[vqa] Based on the image, respond to this question with a short answer: {question}"
         return image, question, candidates, answer
+
 
 class GQAEvalData(torch.utils.data.Dataset):
     def __init__(self, loaded_data, vis_processor, root_path):
@@ -109,7 +119,7 @@ class GQAEvalData(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.loaded_data)
-    
+
     def __getitem__(self, idx):
         ann = self.loaded_data[idx]
         image_id = ann["image"]
@@ -122,6 +132,7 @@ class GQAEvalData(torch.utils.data.Dataset):
 
         return image, question, labels
 
+
 class HMEvalData(torch.utils.data.Dataset):
     def __init__(self, loaded_data, vis_processor, root_path):
         self.loaded_data = loaded_data
@@ -130,7 +141,7 @@ class HMEvalData(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.loaded_data)
-    
+
     def __getitem__(self, idx):
         ann = self.loaded_data[idx]
         image_id = ann["img"]
@@ -143,6 +154,7 @@ class HMEvalData(torch.utils.data.Dataset):
 
         return image, question, labels
 
+
 class VSREvalData(torch.utils.data.Dataset):
     def __init__(self, loaded_data, vis_processor, root_path):
         self.loaded_data = loaded_data
@@ -151,14 +163,16 @@ class VSREvalData(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.loaded_data)
-    
+
     def __getitem__(self, idx):
         ann = self.loaded_data[idx]
         image_path = os.path.join(self.root_path, ann["image"])
         image = Image.open(image_path).convert("RGB")
         image = self.vis_processor(image)
         question = ann["caption"]
-        question = f'[vqa] Based on the image, is this statement true or false? {question}'
-        labels = 'true' if ann["label"] == 1 else 'false'
+        question = (
+            f"[vqa] Based on the image, is this statement true or false? {question}"
+        )
+        labels = "true" if ann["label"] == 1 else "false"
 
         return image, question, labels
